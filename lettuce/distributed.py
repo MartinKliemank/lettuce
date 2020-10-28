c68e4256f24e9d72cd1e301adb86ff21b8f4fc3a
MartinKliemank <martin.kliemank@gmx.de>
2020-10-28 22:06:38 +0100

    - added new classes to _init_ - finished implementation of distributed stream9ng (asynchronous and synchronous) (still needs reporters and boundaries)

--- lettuce/distributed.py
+++ lettuce/distributed.py
@@ -10,13 +10,13 @@ import warnings
 import torch
 import numpy as np
 
+__all__ = ["DistributedSimulation", "DistributedStreaming"]
+
 class DistributedSimulation(Simulation):
 
     def __init__(self, flow, lattice, collision, streaming, rank, size):
         self.rank = rank
         self.size = size
-        self.prev = self.rank - 1 if self.rank != 0 else self.size - 1
-        self.next = self.rank + 1 if self.rank != self.size - 1 else 0
 
         self.flow = flow
         self.lattice = lattice
@@ -24,7 +24,8 @@ class DistributedSimulation(Simulation):
         self.streaming = streaming
         self.i = 0
 
-        grid = flow.grid[int(flow.grid.size(0)*rank/size):int(flow.grid.size(0)*(rank-1)/size)]
+        grid = [x[int(np.floor(flow.grid[0].shape[0]*rank/size)):int(np.floor(flow.grid[0].shape[0]*(rank+1)/size)),...] for x in flow.grid]
+        print(f"Process {rank} covers {int(np.floor(flow.grid[0].shape[0]*rank/size))}:{int(np.floor(flow.grid[0].shape[0]*(rank+1)/size))}!")
         p, u = flow.initial_solution(grid)
         assert list(p.shape) == [1] + list(grid[0].shape), \
             LettuceException(f"Wrong dimension of initial pressure field. "
@@ -42,51 +43,120 @@ class DistributedSimulation(Simulation):
 
         # Define masks, where the collision or streaming are not applied
         x = grid
-        self.no_collision_mask = lattice.convert_to_tensor(np.zeros_like(x[0],dtype=bool))
+        self.no_collision_mask = lattice.convert_to_tensor(np.zeros_like(x[0], dtype=bool))
         no_stream_mask = lattice.convert_to_tensor(np.zeros(self.f.shape, dtype=bool))
 
-#find out which boundaries apply in what region... (input large f and take small part of mask?)
+#find out which boundaries apply in what region... (input large f and take small part of mask?) (NOT TESTED YET!!!!!!)
 
         # Apply boundaries
         self._boundaries = deepcopy(self.flow.boundaries)  # store locally to keep the flow free from the boundary state
         for boundary in self._boundaries:
             if hasattr(boundary, "make_no_collision_mask"):
-                self.no_collision_mask = self.no_collision_mask | boundary.make_no_collision_mask(self.f.shape)
+                self.no_collision_mask = self.no_collision_mask | boundary.make_no_collision_mask(torch.zeros([lattice.Q]+list(flow.grid[0].shape)))[int(np.floor(flow.grid[0].shape[0]*rank/size)):int(np.floor(flow.grid[0].shape[0]*(rank+1)/size)),...]
             if hasattr(boundary, "make_no_stream_mask"):
-                no_stream_mask = no_stream_mask | boundary.make_no_stream_mask(self.f.shape)
+                no_stream_mask = no_stream_mask | boundary.make_no_stream_mask(torch.zeros([lattice.Q]+list(flow.grid[0].shape)))[:,int(np.floor(flow.grid[0].shape[0]*rank/size)):int(np.floor(flow.grid[0].shape[0]*(rank+1)/size)),...]
         if no_stream_mask.any():
             self.streaming.no_stream_mask = no_stream_mask
 
     def step(self, num_steps):
-
-        text = 3
+        """Take num_steps stream-and-collision steps and return performance in MLUPS."""
+        start = timer()
+        if self.i == 0:
+            self._report()
+        for _ in range(num_steps):
+            self.i += 1
+            self.f = self.streaming(self.f)
+            # Perform the collision routine everywhere, expect where the no_collision_mask is true
+            self.f = torch.where(self.no_collision_mask, self.f, self.collision(self.f))
+            for boundary in self._boundaries:
+                self.f = boundary(self.f)
+            self._report()
+        end = timer()
+        seconds = end - start
+        num_grid_points = self.lattice.rho(self.f).numel()
+        mlups = num_steps * num_grid_points / 1e6 / seconds
+        return mlups
 
 
 class DistributedStreaming(StandardStreaming):
     """Standard streaming for distributed simulation, domain is separated along 0th (x)-dimension"""
 
-    def __init__(self, lattice, prev, rank, next):
+    def __init__(self, lattice, rank, size):
         self.lattice = lattice
-        self.prev = prev
+        self.size = size
         self.rank = rank
-        self.next = next
+        self.prev = self.rank - 1 if self.rank != 0 else self.size - 1
+        self.next = self.rank + 1 if self.rank != self.size - 1 else 0
         self._no_stream_mask = None
 
+#maybe make stream only roll again and stream all I's for one direction at the same time?
     def _stream(self, f, i):
-        if self.rank == 0:
-            dist.send(tensor=(f[-1, ...] if self.lattice.e[i, 0] > 0 else f[0, ...]).contiguous(),
-                      dst=self.next if self.lattice.e[i, 0] > 0 else self.prev)
-
-        # add new columns (add 1 coordinate in x at front and back of domain for in / outflow)
-        f = torch.cat((torch.zeros_like(f[i, 0, ...]).unsqueeze(0), f[i], torch.zeros_like(f[i, 0, ...]).unsqueeze(0)), dim=0)
-        # receive inflow from prev / next process into first / last column
-        dist.recv(tensor=(f[0, ...] if self.lattice.e[i, 0] > 0 else f[-1, ...]).contiguous(),
-                  src=self.prev if self.lattice.e[i, 0] > 0 else self.next)
-        # stream normally in f
-        f = torch.roll(f, shifts=tuple(self.lattice.stencil.e[i]), dims=tuple(np.arange(self.lattice.D)))
-        # stream out the outflow that has ended up in the overhanging column
-        if self.rank != 0:
-            dist.send(tensor=(f[-1, ...] if self.lattice.e[i, 0] > 0 else f[0, ...]).contiguous(),
-                      dst=self.next if self.lattice.e[i, 0] > 0 else self.prev)
-
-        return f[1:-2, ...]
+        if self.lattice.e[i, 0] != 0:
+            if 1:
+                f = f[i]
+                output = (f[-1, ...] if self.lattice.e[i, 0] > 0 else f[0, ...]).contiguous()
+                if self.rank != 0:
+                    dist.send(tensor=output,
+                              dst=self.next if self.lattice.e[i, 0] > 0 else self.prev)
+                f = torch.cat((torch.zeros_like(f[0, ...]).unsqueeze(0), f, torch.zeros_like(f[0, ...]).unsqueeze(0)), dim=0)
+                input = torch.zeros_like(f[0, ...])
+                dist.recv(tensor=input.contiguous(),
+                          src=self.prev if self.lattice.e[i, 0] > 0 else self.next)
+                if self.lattice.e[i, 0] > 0:
+                    f[0, ...] = input
+                else:
+                    f[-1, ...] = input
+                f = torch.roll(f, shifts=tuple(self.lattice.stencil.e[i]), dims=tuple(np.arange(self.lattice.D)))
+                if self.rank == 0:
+                    dist.send(tensor=output,
+                              dst=self.next if self.lattice.e[i, 0] > 0 else self.prev)
+                return f[1:-1, ...]
+            if 0:
+                f = f[i]
+                output = (f[-1, ...] if self.lattice.e[i, 0] > 0 else f[0, ...]).detach().clone().contiguous()
+                out = dist.isend(tensor=output,
+                          dst=self.next if self.lattice.e[i, 0] > 0 else self.prev)
+                f = torch.cat((torch.zeros_like(f[0, ...]).unsqueeze(0), f, torch.zeros_like(f[0, ...]).unsqueeze(0)), dim=0)
+                input = torch.zeros_like(f[0, ...])
+                inp = dist.irecv(tensor=input.contiguous(),
+                          src=self.prev if self.lattice.e[i, 0] > 0 else self.next)
+                inp.wait()
+                if self.lattice.e[i, 0] > 0:
+                    f[0, ...] = input
+                else:
+                    f[-1, ...] = input
+                f = torch.roll(f, shifts=tuple(self.lattice.stencil.e[i]), dims=tuple(np.arange(self.lattice.D)))
+                out.wait()
+                return f[1:-1, ...]
+            """
+            if self.rank == 0:
+                tmp = torch.cat((torch.zeros_like(f[i, 0, ...]).unsqueeze(0), f[i], torch.zeros_like(f[i, 0, ...]).unsqueeze(0)),dim=0)
+                tmp = torch.roll(tmp, shifts=tuple(self.lattice.stencil.e[i]), dims=tuple(np.arange(self.lattice.D)))
+                output = (tmp[-1, ...] if self.lattice.e[i, 0] > 0 else tmp[0, ...]).contiguous()
+                #output = (f[i, -1, ...] if self.lattice.e[i, 0] > 0 else f[i, 0, ...]).contiguous()
+                dist.send(tensor=output,
+                          dst=self.next if self.lattice.e[i, 0] > 0 else self.prev)
+    
+            # add new columns (add 1 coordinate in x at front and back of domain for in / outflow)
+            f = torch.cat((torch.zeros_like(f[i, 0, ...]).unsqueeze(0), f[i], torch.zeros_like(f[i, 0, ...]).unsqueeze(0)), dim=0)
+            # receive inflow from prev / next process into first / last column
+            input = torch.zeros_like(f[0, ...])
+            dist.recv(tensor=input.contiguous(),
+                      src=self.prev if self.lattice.e[i, 0] > 0 else self.next)
+            if self.lattice.e[i, 0] > 0:
+                f[0, ...] = input
+            else:
+                f[-1, ...] = input
+            #(f[0, ...] if self.lattice.e[i, 0] > 0 else f[-1, ...])
+            # stream normally in f, outflow ends up in added column, inflow goes into domain, is replaced by 0 in the add. column
+            f = torch.roll(f, shifts=tuple(self.lattice.stencil.e[i]), dims=tuple(np.arange(self.lattice.D)))
+            # stream out the outflow that has ended up in the overhanging column
+            if self.rank != 0:
+                output = (f[-1, ...] if self.lattice.e[i, 0] > 0 else f[0, ...]).contiguous()
+                dist.send(tensor=output,
+                          dst=self.next if self.lattice.e[i, 0] > 0 else self.prev)
+    
+            return f[1:-1, ...]
+            """
+        else:
+            return torch.roll(f[i], shifts=tuple(self.lattice.stencil.e[i]), dims=tuple(np.arange(self.lattice.D)))
