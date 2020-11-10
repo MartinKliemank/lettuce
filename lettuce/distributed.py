c6d563181dab7781109289506386cf22d798459b
MartinKliemank <martin.kliemank@gmx.de>
2020-11-10 06:56:23 -0800

    - diagnostic prints to get reassemble running on several gpus

--- lettuce/distributed.py
+++ lettuce/distributed.py
@@ -19,6 +19,7 @@ def reassemble(flow, lattice, tensor, rank, size):
         assembly = tensor
         for i in range(1, size):
             input = torch.zeros(list(flow.grid[0].shape), dtype=lattice.dtype)[int(np.floor(flow.grid[0].shape[0] * i / size)):int(np.floor(flow.grid[0].shape[0] * (i + 1) / size)), ...].contiguous()
+            print(f"Prepped {input.shape} for process {i}")
             dist.recv(tensor=input, src=i)
             print(f"Recieved {input.shape} from process {i}")
             assembly = torch.cat((assembly, input), dim=0)
